bd4e1546540223da2f10b20d90d54c3fed183f88
aaronsaikovski <asaikovski@outlook.com>
2024-03-17 17:14:29 +1100

    fixed sub regex

--- pkg/utils/subscription.go
+++ pkg/utils/subscription.go
@@ -8,4 +8,5 @@ import (
 func CheckValidSubscriptionID(subscriptionID string) bool {
 	pattern := regexp.MustCompile(`^(\{{0,1}([0-9a-fA-F]){8}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){12}\}{0,1})$`)
 	return pattern.MatchString(subscriptionID)
+
 }
