9df89fec29c22b24b4934c035e765252fce1be9c
aaronsaikovski <asaikovski@outlook.com>
2024-03-18 14:51:02 +1100

    added auth

--- internal/pkg/azure/auth/auth.go
+++ internal/pkg/azure/auth/auth.go
@@ -1,17 +1,42 @@
 package auth
 
 import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/internal/exported"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 )
 
-// AuthtoAzure returns a DefaultAzureCredential and an error.
+// GetAzureDefaultCredential retrieves the default Azure credential.
 //
 // No parameters.
-// Return type (*azidentity.DefaultAzureCredential, error).
-func AuthtoAzure() (*azidentity.DefaultAzureCredential, error) {
+// Returns a pointer to azidentity.DefaultAzureCredential and an error.
+
+func GetAzureDefaultCredential() (*azidentity.DefaultAzureCredential, error) {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		return nil, err
 	}
 	return cred, nil
+}
+
+// GetAzCachedAccessToken retrieves an Azure cached access token.
+//
+// ctx - the context in which the function is being called.
+// *exported.AzCachedAccessToken - returns a pointer to an Azure cached access token.
+func GetAzCachedAccessToken(ctx context.Context) *exported.AzCachedAccessToken {
+
+	cred, err := GetAzureDefaultCredential()
+	if err != nil {
+		return nil
+	}
+	token, err := cred.GetToken(ctx, policy.TokenRequestOptions{})
+
+	if err != nil {
+		return nil
+	}
+
+	return &token
+
 }
